8a5c1b956fe09a015199f92acdc9c9560fa245a1
arodrigue003 <arodrigue003@enseirb-matmeca.fr>
2018-01-14 20:40:47 -0100

    modifying tx & adding bigtext.txt

--- src/python/data/tx.py
+++ src/python/data/tx.py
@@ -5,7 +5,19 @@ import serial
 import time
 import base64
 import hashlib
+from math import floor, ceil
 
+def read_in_chunks(file_object, chunk_size=1024):
+    """Lazy function (generator) to read a file piece by piece.
+    Default chunk size: 1k."""
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+def get_sleep_time(filesize):
+    return 0
 dirpath = os.path.dirname(os.path.realpath(__file__))
 sys.path.append( dirpath + "/../modules")
 from baudrate import checkBaudrateHelp
@@ -27,21 +39,53 @@ else:
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial("/dev/ttyAMA0", bd, timeout=1.0)
 
-size = os.path.getsize(filename)*8
-print("Expected transmission time: " + str((size/bd)*1.5) + "s")
+sizeB = os.path.getsize(filename)
+size = sizeB *8 
+sizeB = ceil(sizeB / 3) * 4
 
-string = base64.b64encode(open(filename, "rb").read())
-md5 = hashlib.md5(string).hexdigest()
+string = ""
+encoded = ""
+Ubytes_sent = 0
+Tbytes_sent = 0
+
+fd = open(filename, "rb")
+
+print("Expected transmission time: " + str((size/bd)*2) + "s")
 
 start = time.time();
 
-bytes_sent = ser.write(string)
-ser.write("\n")
+for chunk in read_in_chunks(fd,33333):
+	encoded = base64.b64encode(chunk)
+	string += encoded
+	Ubytes_sent += ser.write(encoded)
+        ser.write("\n")
+        Tbytes_sent += 1
+#######################
+        #md5 = hashlib.md5(encoded).hexdigest()
+        #ser.write(md5)
+        #ser.write("\n")
+        #Tbytes_sent += len(md5) + 1
+#######################
+        print(str(floor(100*Ubytes_sent/sizeB)) + "% ")
+        if bd > 150000: #completement arbitraire, il faudrait une formule en fonction bd et sizeB
+            time.sleep(0.6)
+
+ser.write("<\n")
+md5 = hashlib.md5(string).hexdigest()
 md5_len = ser.write(md5)
 ser.write("\n")
+Tbytes_sent += 2 + len(md5) + 1
+Tbytes_sent += Ubytes_sent
 
-end = time.time();
+end = time.time()
+elapsed = end-start
 
-print("Envoyé " + str(bytes_sent) + " + " + str(md5_len) + " octets en " + str(end - start) + " sec")
+print("Total " + 
+        str(Tbytes_sent) + " octets en " + str(elapsed) + " sec: "+
+        str(Tbytes_sent/elapsed) + " o/s")
+
+print("Utile: " + str(Tbytes_sent) + "/" + str(Ubytes_sent) + ": "+
+        str(100*(Ubytes_sent/float(Tbytes_sent))) + "%. " +
+        str(Ubytes_sent/elapsed) + " o/s")
 print("md5: " + md5)
 
